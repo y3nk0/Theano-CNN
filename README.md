4d121b50a08d3b2a2b0ea0961562358519477f23
Konstantinos Skianis <rob.cs.aueb@gmail.com>
2018-06-21 16:41:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+## Convolutional Neural Networks for Sentence Classification
+Code for reproducing all results for all datasets by the paper [Convolutional Neural Networks for Sentence Classification](http://arxiv.org/abs/1408.5882) (EMNLP 2014).
+
+### Requirements
+Code is written in Python (3.5) and requires Theano (1.0.2).
+
+Using the pre-trained `word2vec` vectors will also require downloading the binary file from
+https://code.google.com/p/word2vec/
+
+
+### Data Preprocessing
+To process the raw data, run
+
+```
+python process_data.py
+```
+
+where path points to the word2vec binary file (i.e. `GoogleNews-vectors-negative300.bin` file).
+This will create a pickle object called `mr.p` in the same folder, which contains the dataset
+in the right format.
+
+### Running the models (CPU)
+Example commands:
+
+```
+THEANO_FLAGS=mode=FAST_RUN,device=cpu,floatX=float32 python3.5 conv_net_sentence.py -nonstatic -rand
+THEANO_FLAGS=mode=FAST_RUN,device=cpu,floatX=float32 python3.5 conv_net_sentence.py -static -word2vec
+THEANO_FLAGS=mode=FAST_RUN,device=cpu,floatX=float32 python3.5 conv_net_sentence.py -nonstatic -word2vec
+```
+
+This will run the CNN-rand, CNN-static, and CNN-nonstatic models respectively in the paper.
+
+### Using the GPU
+GPU will result in a good 10x to 20x speed-up, so it is highly recommended.
+To use the GPU, simply change `device=cpu` to `device=gpu` (or whichever gpu you are using).
+For example:
+```
+THEANO_FLAGS=mode=FAST_RUN,device=cuda0,floatX=float32 python3.5 conv_net_sentence.py -nonstatic -word2vec
+```
+
+### Other Implementations
+#### TensorFlow
+[Denny Britz](http://www.wildml.com) has an implementation of the model in TensorFlow:
+
+https://github.com/dennybritz/cnn-text-classification-tf
+
+He also wrote a [nice tutorial](http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow) on it, as well as a general tutorial on [CNNs for NLP](http://www.wildml.com/2015/11/understanding-convolutional-neural-networks-for-nlp).
+
+#### Torch
+[HarvardNLP](http://harvardnlp.github.io/) group has an implementation in Torch.
+
+https://github.com/harvardnlp/sent-conv-torch
+
+### Hyperparameters
+At the time of my original experiments I did not have access to a GPU so I could not run a lot of different experiments.
+Hence the paper is missing a lot of things like ablation studies and variance in performance, and some of the conclusions
+were premature (e.g. regularization does not always seem to help).
+
+Ye Zhang has written a [very nice paper](http://arxiv.org/abs/1510.03820) doing an extensive analysis of model variants (e.g. filter widths, k-max pooling, word2vec vs Glove, etc.) and their effect on performance.
---
+++ process_data.py
@@ -0,0 +1,220 @@
+import numpy as np
+import _pickle as cPickle
+from collections import defaultdict
+import sys, re
+import pandas as pd
+
+def build_data_cv(data_file, cv=10, clean_string=True):
+    """
+    Loads data and split into 10 folds.
+    """
+    revs = []
+
+    vocab = defaultdict(float)
+    with open(data_file, "r",encoding='latin1') as f:
+        for line in f:
+            rev = []
+            rev.append(line[2:].strip())
+            if clean_string:
+                orig_rev = clean_str(" ".join(rev))
+            else:
+                orig_rev = " ".join(rev).lower()
+            words = set(orig_rev.split())
+            for word in words:
+                vocab[word] += 1
+            datum  = {"y":int(line[0]),
+                      "text": orig_rev,
+                      "num_words": len(orig_rev.split()),
+                      "split": np.random.randint(0,cv)}
+            revs.append(datum)
+
+    return revs, vocab
+
+def build_data_split(data_folder, clean_string=True):
+    """
+    Loads data for pre-split datasets.
+    """
+    revs = []
+    # pos_file = data_folder[0]
+    # neg_file = data_folder[1]
+
+    train_file = data_folder[0]
+    test_file = data_folder[1]
+    dev_file = data_folder[2]
+
+    train_revs = []
+    test_revs = []
+    dev_revs = []
+
+    vocab = defaultdict(float)
+    with open(train_file, "r") as f:
+        for line in f:
+            rev = []
+            rev.append(line[2:].strip())
+            if clean_string:
+                if "stsa" in train_file:
+                    orig_rev = clean_str_sst(" ".join(rev))
+                else:
+                    orig_rev = clean_str(" ".join(rev))
+            else:
+                orig_rev = " ".join(rev).lower()
+            words = set(orig_rev.split())
+            for word in words:
+                vocab[word] += 1
+            datum = {"y":int(line[0]),
+                      "text": orig_rev,
+                      "num_words": len(orig_rev.split())}
+            train_revs.append(datum)
+
+    with open(dev_file, "r") as f:
+        for line in f:
+            rev = []
+            rev.append(line[2:].strip())
+            if clean_string:
+                if "stsa" in dev_file:
+                    orig_rev = clean_str_sst(" ".join(rev))
+                else:
+                    orig_rev = clean_str(" ".join(rev))
+            else:
+                orig_rev = " ".join(rev).lower()
+            words = set(orig_rev.split())
+            for word in words:
+                vocab[word] += 1
+            datum = {"y":int(line[0]),
+                      "text": orig_rev,
+                      "num_words": len(orig_rev.split())}
+            dev_revs.append(datum)
+
+    with open(test_file, "r") as f:
+        for line in f:
+            rev = []
+            rev.append(line[2:].strip())
+            if clean_string:
+                if "stsa" in test_file:
+                    orig_rev = clean_str_sst(" ".join(rev))
+                else:
+                    orig_rev = clean_str(" ".join(rev))
+            else:
+                orig_rev = " ".join(rev).lower()
+
+            datum = {"y":int(line[0]),
+                      "text": orig_rev,
+                      "num_words": len(orig_rev.split())}
+            test_revs.append(datum)
+
+    return train_revs, test_revs, dev_revs, vocab
+
+def get_W(word_vecs, k=300):
+    """
+    Get word matrix. W[i] is the vector for word indexed by i
+    """
+    vocab_size = len(word_vecs)
+    word_idx_map = dict()
+    W = np.zeros(shape=(vocab_size+1, k), dtype='float32')
+    W[0] = np.zeros(k, dtype='float32')
+    i = 1
+    for word in word_vecs:
+        W[i] = word_vecs[word]
+        word_idx_map[word] = i
+        i += 1
+    return W, word_idx_map
+
+def load_bin_vec(fname, vocab):
+    """
+    Loads 300x1 word vecs from Google (Mikolov) word2vec
+    """
+    word_vecs = {}
+
+    from gensim.models import KeyedVectors
+    model = KeyedVectors.load_word2vec_format(fname, binary=True)  # C binary format
+
+    for word in vocab:
+        if word in model.wv.vocab:
+            word_vecs[word]=model[word]
+
+    del model
+
+    return word_vecs
+
+def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
+    """
+    For words that occur in at least min_df documents, create a separate word vector.
+    0.25 is chosen so the unknown vectors have (approximately) same variance as pre-trained ones
+    """
+    for word in vocab:
+        if word not in word_vecs and vocab[word] >= min_df:
+            word_vecs[word] = np.random.uniform(-0.25,0.25,k)
+
+def clean_str(string, TREC=False):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Every dataset is lower cased except for TREC
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip() if TREC else string.strip().lower()
+
+def clean_str_sst(string):
+    """
+    Tokenization/string cleaning for the SST dataset
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+if __name__=="__main__":
+
+    # dataset = "../data/rt-polarity.all"
+    #dataset = "../data/custrev.all"
+    #dataset = "../data/mpqa.all"
+    #dataset = "../data/subj.all"
+
+    # dataset = "../data/stsa.binary"
+    dataset = "../data/stsa.fine"
+    #dataset = "../data/TREC.fine"
+
+    dataset_split = "split"
+
+    w2v_file = "../GoogleNews-vectors-negative300.bin"
+
+    print("loading data...")
+    if dataset_split=="cv":
+        revs, vocab = build_data_cv(dataset, cv=10, clean_string=True)
+        max_l = np.max(pd.DataFrame(revs)["num_words"])
+        print("number of sentences: " + str(len(revs)))
+    else:
+        data_folder = [dataset+".train",dataset+".test",dataset+".dev"]
+        train_revs, test_revs, dev_revs, vocab = build_data_split(data_folder, clean_string=True)
+        max_l = np.max(pd.DataFrame(train_revs)["num_words"])
+
+    print("data loaded!")
+
+    print("vocab size: " + str(len(vocab)))
+    print("max sentence length: " + str(max_l))
+    print("loading word2vec vectors...")
+    w2v = load_bin_vec(w2v_file, vocab)
+    print("word2vec loaded!")
+    print("num words already in word2vec: " + str(len(w2v)))
+    add_unknown_words(w2v, vocab)
+    W, word_idx_map = get_W(w2v)
+    rand_vecs = {}
+    add_unknown_words(rand_vecs, vocab)
+    W2, _ = get_W(rand_vecs)
+
+    if dataset_split=="cv":
+        cPickle.dump([revs, W, W2, word_idx_map, vocab, max_l], open("mr.p", "wb"))
+    else:
+        cPickle.dump([train_revs, dev_revs, test_revs, W, W2, word_idx_map, vocab, max_l], open("mr_split.p", "wb"))
+
+    print("dataset created!")
